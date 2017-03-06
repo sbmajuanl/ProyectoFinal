4bbc82c80999bd576be2f5470dd827f2b0afc88a
Brilly Majuan <sbmajuanl@gmail.com>
2017-03-06 18:45:18 -0500

    subiendo cambios

---
+++ assets/main.js/coder_lab.js
@@ -0,0 +1,14 @@
+var coders = [ { nombre : "Meche", apellido : "Zubieta",  promocion : "1era. promoción", estado : "Trabajando" },
+			 { nombre : "Arabela", apellido : "Rojas", promocion : "2da. promoción", estado : "Trabajando" },
+			 { nombre : "Maria", apellido : "Rosán",  promocion : "2da. promoción", estado : "Trabajando" },
+			 { nombre : "Guadalupe", apellido : "Racio",  promocion : "3era. promoción", estado : "Trabajando" },
+			 { nombre : "Sara", apellido : "Casa",  promocion : "3era. promoción", estado : "Trabajando" },
+			 { nombre : "Daniella", apellido : "Durán",  promocion : "4ta. promoción", estado : "Trabajando" },
+			 { nombre : "Michelle", apellido : "Seguil",  promocion : "5ta. promoción", estado : "Trabajando" },
+			 { nombre : "Diana", apellido : "Navarro",  promocion : "5ta. promoción", estado : "Trabajando" } ];
+
+var ex_alumnas = document.getElementsByClassName('ex_alumnas');
+
+  ex_alumnas.forEach(function(c){
+  this.innerHTML = coders[c].nombre +"<br>"+coders[c].apellido +"<br>"+coders[c].promoción +"<br>"+coders[c].estado;
+  })
---
+++ assets/main.js/main.js
@@ -0,0 +1,48 @@
+
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
+
+var sprint1 = document.getElementById('sprint1');
+var sprint2 = document.getElementById('sprint2');
+var sprint3 = document.getElementById('sprint3');
+var datos = document.getElementById('datos');
+
+
+sprint1.addEventListener('click', function(){
+datos.innerHTML = "<h3>"+"Sprint1: Introducción a Javascript"+"</h3>"+
+                    "<ol>"+
+                        "<li>"+"Fundamentos de Programación"+"</li>"+
+                        "<li>"+"Funciones"+"</li>"+
+                        "<li>"+"Arreglos"+"</li>"+
+                        "<li>"+"Objetos"+"</li>"+
+                      "</ol>"
+});
+
+sprint2.addEventListener('click', function(){
+datos.innerHTML = "<h3>"+"Sprint2: Introducción al desarrollo web y construcción de páginas web"+"</h3>"+
+                    "<ol>"+
+                        "<li>"+"Ciclo de vida del desarrollo web"+"</li>"+
+                        "<li>"+"HTML y Javascript en el navegador"+"</li>"+
+                        "<li>"+"Manejo de comandos básicos de git en la terminal"+"</li>"+
+                        "<li>"+"Pruebas unitarias"+"</li>"+
+                        "<li>"+"Git/Rosolución de conflictos/Branching model"+"</li>"+
+                        "<li>"+"DOM"+"</li>"+
+                    "</ol>"
+                });
+
+sprint3.addEventListener('click', function(){
+datos.innerHTML = "<h3>"+"Sprint3"+"</h3>"+"</br>"+"</br>"+"No hay lecciones todavía"+
+});
